43a09ccb7a3eb9495bacff2b2f8795fdd9fd5df3
HarrisChang <jone09@outlook.com>
2017-02-07 14:06:19 +0800

    0207W

---
+++ entry.js
@@ -0,0 +1,5 @@
+/**
+ * Created by admin on 2017/2/7.
+ */
+document.write("It works");
+document.write(require("./module.js")) //添加模块
---
+++ module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 2017/2/7.
+ */
+module.exports = "It works from module.js"
