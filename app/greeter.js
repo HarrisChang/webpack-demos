aed2e2c2fc88220e985b682f24a3d4600426059f
HarrisChang <jone09@outlook.com>
2017-02-10 14:56:24 +0800

    init floder

--- app/greeter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by admin on 2017/2/9.
- */
-var config = require("./config.json")
-module.exports = function(){
-    var greet = document.createElement('div');
-    greet.textContent = config.greetText;
-    return greet;
-}
--- app/main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by admin on 2017/2/9.
- */
-
-var greeter = require('./greeter.js');
-document.getElementById('root').appendChild(greeter());
