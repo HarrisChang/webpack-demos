7f33d600a32bb542374e6ec6dd626b087e07a6a2
HarrisChang <jone09@outlook.com>
2017-02-10 17:32:51 +0800

    add 2 demo

---
+++ demo02/main2.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 2017/2/10.
+ */
+document.write("<h1>Hello Webpack!</h1>");
---
+++ demo02/webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 2017/2/10.
+ */
+module.exports = {
+    entry: {
+        bundle1: './main1.js',
+        bundle2: './main2.js'
+    },
+    output: {
+        filename: '[name].js'
+    }
+}
