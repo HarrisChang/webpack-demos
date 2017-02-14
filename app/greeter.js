6aa33f3e916ef9984723df545b25251d8961f5bc
HarrisChang <jone09@outlook.com>
2017-02-14 09:27:32 +0800

    :broken_heart:

---
+++ app/greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 2017/2/14.
+ */
+module.exports = function(){
+    var greet = document.createElement('div');
+    greet.textContent = 'Hi there and greetings!';
+    return greet;
+}
---
+++ app/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by admin on 2017/2/14.
+ */
+var greeter = require('./greeter');
+document.getElementById('root').appendChild(greeter());
