749af933643de1b4a767c99768df0bf9e870934a
HarrisChang <jone09@outlook.com>
2017-02-15 17:52:58 +0800

    0215W

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by admin on 2017/2/15.
+ */
+const a = "Hello Webpack";
+document.write(a);
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: ['babel?preset=es2015']
             },
             {
