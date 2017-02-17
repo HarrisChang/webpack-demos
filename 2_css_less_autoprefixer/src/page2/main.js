40e08c0da53f21d9143adda87a4da18898ea14cf
HarrisChang <jone09@outlook.com>
2017-02-17 10:59:31 +0800

    add demo01 css_less_autoprefixer

---
+++ 2_css_less_autoprefixer/src/page2/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 2017/2/16.
+ */
+import './style.less'
+
+import { log } from '../common/index'
+
+log('this is page2')
---
+++ 2_css_less_autoprefixer/webpack.config.js
@@ -0,0 +1,58 @@
+var path = require('path')
+var ROOT = path.resolve(__dirname)
+var autoprefixer = require('autoprefixer')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var extractCSS = new ExtractTextPlugin('[name].css')
+
+module.exports = {
+    entry: {
+        'page1/main': ROOT + '/src/page1/main',
+        'page2/main': ROOT + '/src/page2/main'
+    },
+    output: {
+        path: ROOT + '/dist',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function(){
+                                return [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function(){
+                                return [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        extractCSS
+    ]
+}
